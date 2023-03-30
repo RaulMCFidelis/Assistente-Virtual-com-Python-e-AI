9ef47266074763bb6ac526a7c76ff75cb55c3d31
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-03-29 22:53:31 -0300

    Reconhecendo-Voz-Microfone-IA-Google

---
+++ index.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+mic = sr.Recognizer()
+
+with sr.Microphone() as source:
+    mic.adjust_for_ambient_noise(source)
+
+    print("Vamos começar, fale alguma coisa...")
+
+    audio = mic.listen(source)
+
+    try:
+        frase = mic.recognize_google(audio, language='pt-BR')
+        print("Você falou: "+frase)
+
+    except sr.UnknownValueError:
+        print("ops, algo deu errado.")    
